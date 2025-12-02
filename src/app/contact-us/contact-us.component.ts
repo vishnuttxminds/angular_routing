44f62d892aedbc6af503e176af1c42f23cc0ec38
vishnutt <vishnutt@xminds.com>
2025-12-02 17:14:55 +0530

    navigation

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router , ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
+  router : Router = inject(Router)
+  active : ActivatedRoute = inject(ActivatedRoute)
+
+  // constructor(private router: Router) {}
+
+  navihateToNext(){
+    console.log("NExt click activated")
+    // this.router.navigate(['about'], {relativeTo : this.active})
+    this.router.navigateByUrl('about')
+  }
 
 }
